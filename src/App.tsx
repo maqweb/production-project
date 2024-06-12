d8227426610f9dda1118fb67b2155b7130a3d999
Vitaly Balikhin <balikhinv@bk.ru>
2024-06-12 13:10:47 +0300

    code splitting - lazy / suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss'
+import {AboutLazy} from "./pages/About/About.lazy";
+import {MainLazy} from "./pages/Main/Main.lazy";
 
 const App = () => {
     return (
         <div className={'app'}>
-            React
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutLazy/>}/>
+                    <Route path={'/'} element={<MainLazy/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
