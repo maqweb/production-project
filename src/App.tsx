52e0b8fe1c47a629ca063a6b8ee2b6c8a49cdea7
Vitaly Balikhin <balikhinv@bk.ru>
2024-06-13 23:58:01 +0300

    organization styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss'
+import './styles/index.scss'
 import {AboutLazy} from "./pages/About/About.lazy";
 import {MainLazy} from "./pages/Main/Main.lazy";
+import {Theme} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className={'app'}>
+        <div className={`app ${theme}`}>
+            <div>
+                <button onClick={toggleTheme}>{theme === Theme.DARK ? Theme.LIGHT : Theme.DARK}</button>
+            </div>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
